3f74f64649e4e137d6e392fb819f1ef2b80d8e8b
wang-y-z <wangyz.xjtu@gmail.com>
2023-04-17 06:06:09 +0000

    autotune profile

--- python/triton/runtime/autotuner.py
+++ python/triton/runtime/autotuner.py
@@ -83,6 +83,7 @@ class Autotuner(KernelInterface):
 
     def run(self, *args, **kwargs):
         self.nargs = dict(zip(self.arg_names, args))
+        configs_res = '\n'
         if len(self.configs) > 1:
             all_args = {**self.nargs, **kwargs}
             _args = []
@@ -97,7 +98,22 @@ class Autotuner(KernelInterface):
                 timings = {config: self._bench(*args, config=config, **kwargs)
                            for config in pruned_configs}
                 bench_end = time.time()
+                sorted_configs = sorted(timings.keys(), key=lambda x: timings[x][1])
+                number_top = 1
+                for config in sorted_configs:
+                    configs_res += "|#| top" 
+                    configs_res += str(number_top) if number_top <= 5 else "N"
+                    configs_res += " "
+                    number_top += 1
+                    configs_res += config.__str__()
+                    configs_res += "    "
+                    configs_res += str(timings[config][1])
+                    configs_res += "\n"
+                print("|#| =====================")
+                print("|#| configs bench : \n", configs_res)
                 self.bench_time = bench_end - bench_start
+                print("|#| bench_time", self.bench_time)
+                print("|#| =====================")
                 self.cache[key] = builtins.min(timings, key=timings.get)
                 self.hook(args)
                 self.configs_timings = timings
